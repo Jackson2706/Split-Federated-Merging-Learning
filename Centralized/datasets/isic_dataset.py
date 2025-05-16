30ae197889b826607adc0166ac35a0d119436a25
nguyenha_ra_cei_serverless <nguyenha@jackson.devworks04>
2025-05-16 09:28:11 +0700

    refactor datasets dir

---
+++ Centralized/datasets/isic_dataset.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+class ISICDataset(Dataset):
+    def __init__(self, csv_file, img_dir, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.transform = transform
+
+        # Optional: Map labels to integer indices
+        self.label_map = {label: idx for idx, label in enumerate(self.annotations['dx'].unique())}
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, idx):
+        img_name = self.annotations.iloc[idx]['image_id'] + '.jpg'
+        img_path = os.path.join(self.img_dir, img_name)
+        image = Image.open(img_path).convert("RGB")
+
+        label_str = self.annotations.iloc[idx]['dx']
+        label = self.label_map[label_str]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
