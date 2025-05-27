9999ad589f72086448f6152d18e1ca79fdad3f0c
Jackson2706 <dung27062002@gmail.com>
2025-05-27 18:09:58 +0700

    implement HierFL

---
+++ HierFL/models/constant.py
@@ -0,0 +1,20 @@
+from .CNNCifar import CNNCifar
+from .CNNFashion_Mnist import CNNFashion_Mnist
+from .CNNMnist import CNNMnist
+from .MLP import MLP
+
+model_dataset_map = {
+    "cnn":{
+        "mnist": CNNMnist,
+        "fmnist": CNNFashion_Mnist,
+        "cifar": CNNCifar
+    },
+    "mlp": MLP
+}
+
+
+def get_model(model, dataset):
+    try:
+        return model_dataset_map[model][dataset]
+    except:
+        exit("Error: unrecognized model")
