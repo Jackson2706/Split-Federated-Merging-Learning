a2ef34c8bacf43a26cf39b323bd196feacbc7fe9
Jackson2706 <dung27062002@gmail.com>
2025-05-27 11:29:15 +0700

    Update Federated Baseline

---
+++ Federated/clients/__init__.py
@@ -0,0 +1,2 @@
+from .test import test_inference
+from .constant import get_client_update_strategy
---
+++ Federated/server/FedSGDAggregator.py
@@ -0,0 +1,5 @@
+from .FedAvgAggregator import FedAvgAggregator
+
+class FedSGDAggregator(FedAvgAggregator):
+    # FedSGD có thể kế thừa FedAvgAggregator vì cách aggregation giống nhau
+    pass
